cdf63d23366714389fa4ba331a31e7433cdf132a
Nicklas Pascetti <Npascetti@gmail.com>
2017-11-04 14:01:38 -0600

    sdded border & margin to navbar

--- index.php
+++ index.php
@@ -14,23 +14,35 @@
 	</head>
 	<body>
 		<div class="container">
-			<nav class="navbar">
-				<div class="col-8">Title</div>
-				<div class="col-1">Link 1</div>
-				<div class="col-1">Link 2</div>
-				<div class="col-1">Link 3</div>
-				<div class="col-1">Link 4</div>
+			<nav class="navbar navbar-link bg-light mb-3 mt-3 border border-dark rounded">
+				<div class="col-8"><h3>Title</h3></div>
+				<div class="col-1"><a href="mylink">Link 1</a></div>
+				<div class="col-1"><a href="mylink">Link 2</a></div>
+				<div class="col-1"><a href="mylink">Link 3</a></div>
+				<div class="col-1"><a href="mylink">Link 4</a></div>
 			</nav>
 		</div>
 		<div class="container">
 			<div class="jumbotron">
-				<h1 class="display-3">Hello, world!</h1>
+				<h1 class="display-2">Hello, world!</h1>
 				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 				<hr class="my-4">
 				<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
 				<p class="lead">
 					<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 				</p>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-3">
+					<div class="thumbnail">
+						<img src="http://www.fillmurray.com/200/300" alt="filler" style="width:100% height:50%"/>
+					</div>
+				</div>
+				<div class="col-9">
+					<p>Lorem Ipusm</p>
+				</div>
 			</div>
 		</div>
 	</body>
