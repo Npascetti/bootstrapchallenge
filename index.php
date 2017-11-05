404ff5b53973391a2eabff21195d69e390058c85
Nick Pascetti <npascetti@cnm.edu>
2017-11-04 21:22:50 -0600

    altered middle image and p to scale correctly

--- index.php
+++ index.php
@@ -43,13 +43,13 @@
 				</p>
 			</div>
 		</div>
-		<div class="container-fluid bg-secondary p-5 mb-5">
+		<div class="container-fluid bg-secondary p-5 mb-5 d-flex-md flex-column">
 			<div class="container">
 			<div class="row">
-				<div class="col-3 d-flex align-items-center">
-						<img src="http://www.fillmurray.com/200/200" alt="filler" class="img-responsive img-thumbnail col-xs-12 border-0""/>
+				<div class="col-md-3 d-flex align-items-center justify-content-center ">
+						<img src="http://www.fillmurray.com/200/200" alt="filler" class="img-responsive col-xs-12""/>
 				</div>
-				<div class="col-9"">
+				<div class="col-md-9"">
 					<p>It's a very cold picture, I may have to go get my coat. It's about to freeze me to death. You can spend all day playing with mountains. I started painting as a hobby when I was little. I didn't know I had any talent. I believe talent is just a pursued interest. Anybody can do what I do. You can do it. Put it in, leave it alone. And just raise cain. If you didn't have baby clouds, you wouldn't have big clouds. And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen.</p>
 					<p>The only thing worse than yellow snow is green snow. Remember how free clouds are. They just lay around in the sky all day long. These trees are so much fun. I get started on them and I have a hard time stopping. We don't have anything but happy trees here. We'll do another happy little painting. In life you need color. This is truly an almighty mountain. Let's do it again then, what the heck. This is your world.  </p>
 				</div>
